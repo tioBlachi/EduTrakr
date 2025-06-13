63f51b1d1aaaef7511084b3d8bc4021037e752d1
Blas <blasantunez310@gmail.com>
2025-06-13 16:58:05 -0400

    created users, students and instructor table for login
    edutrakr.db file created

--- database.py
+++ database.py
@@ -4,4 +4,37 @@ Database Logic Module (To be implemented)
 This file will manage all database-related operations such as reading,
 writing, and updating study session data.
 """
-import sqlite3
+import sqlite3 as sq3
+
+print("Running database")
+
+conn = sq3.connect("edutrakr.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users (
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               email TEXT UNIQUE NOT NULL,
+               passowrd TEXT NOT NULL,
+               role TEXT CHECK(role IN ('student', 'instructor')) NOT NULL
+               )
+               """)
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS students (
+    user_id INTEGER PRIMARY KEY,
+    name TEXT,
+    FOREIGN KEY(user_id) REFERENCES users(id)
+)
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS instructors (
+    user_id INTEGER PRIMARY KEY,
+    name TEXT,
+    FOREIGN KEY(user_id) REFERENCES users(id)
+)
+""")
+
+conn.commit()
+conn.close()
